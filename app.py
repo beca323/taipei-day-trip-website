996f340d4c21bbae4dc2217a583db6268d80bae2
Rebecca_mac <bcwu323@gmail.com>
2021-04-20 15:31:48 +0800

    2api and buildsql.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import *
+# 載入Flask
+from flask import Flask, redirect, request, render_template, session, jsonify, json
+import mysql.connector
+
+mydb = mysql.connector.connect(host='localhost',
+                               user='root',
+                               password='password',
+                               database='website')
+
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["JSON_SORT_KEYS"] = False
 
 
 # Pages
@@ -24,6 +34,70 @@ def booking():
 @app.route("/thankyou")
 def thankyou():
     return render_template("thankyou.html")
+
+
+@app.route('/api/attractions')
+def attractions():
+    page = request.args.get('page', 0)
+    keyword = request.args.get('keyword', '')
+    keyword = '%' + keyword + '%'
+    print(keyword)
+    nextPage = int(page) * 12
+    dataNumPage = 12
+    sql = 'SELECT * FROM (SELECT _id,stitle,CAT2,xbody,address,info,MRT,latitude,longitude,file FROM taipei order by _id LIMIT %s,%s) as a WHERE stitle like %s '
+    val = (nextPage, dataNumPage, keyword)
+    mycursor.execute(sql, val)
+    sqldata = mycursor.fetchall()
+    print(len(sqldata))
+    myresult = [''] * (len(sqldata))
+    for k in range(0, len(sqldata)):
+        imagesx1 = sqldata[k][9].split(',')
+        myresult[k] = {
+            'id': sqldata[k][0],
+            'name': sqldata[k][1],
+            'category': sqldata[k][2],
+            'description': sqldata[k][3],
+            'address': sqldata[k][4],
+            'transport': sqldata[k][5],
+            'mrt': sqldata[k][6],
+            'latitude': sqldata[k][7],
+            'longitude': sqldata[k][8],
+            'images': [imagesx1[0]]
+        }
+    return {'nextPage': nextPage, 'data': myresult}
+
+
+@app.route('/api/attractions/<int:attractionid>')
+def attractionsID(attractionid):
+    sql = 'SELECT _id,stitle,CAT2,xbody,address,info,MRT,latitude,longitude,file FROM taipei WHERE _id=%s'
+    val = (attractionid, )
+    mycursor.execute(sql, val)
+    sqldata = mycursor.fetchall()
+    myresult = ['']
+    imagesx1 = sqldata[0][9].split(',')
+    myresult[0] = {
+        'id': sqldata[0][0],
+        'name': sqldata[0][1],
+        'category': sqldata[0][2],
+        'description': sqldata[0][3],
+        'address': sqldata[0][4],
+        'transport': sqldata[0][5],
+        'mrt': sqldata[0][6],
+        'latitude': sqldata[0][7],
+        'longitude': sqldata[0][8],
+        'images': [imagesx1[0]]
+    }
+    return {'data': myresult}
+
+
+@app.errorhandler(404)
+def internal_error400(error):
+    return {'error': True, "message": "景點編號不正確"}
+
+
+@app.errorhandler(500)
+def internal_error500(error):
+    return {'error': True, "message": "自訂的錯誤訊息"}
 
 
 app.run(port=3000)
---
+++ buildsql.py
@@ -0,0 +1,80 @@
+import mysql.connector
+import json
+
+mydb = mysql.connector.connect(host='localhost',
+                               user='root',
+                               password='password',
+                               database='website')
+
+mycursor = mydb.cursor()
+# sql = 'INSERT INTO taipei (info	,stitle	,xpostDate) VALUES (%s, %s, %s)'
+
+with open('./data/taipei-attractions.json') as f:
+    data = json.load(f)
+
+item = [''] * 21
+itemContent = [''] * 21
+
+item[0] = 'info'
+item[1] = 'stitle'
+item[2] = 'xpostDate'
+item[3] = 'longitude'
+item[4] = 'REF_WP'
+item[5] = 'avBegin'
+item[6] = 'langinfo'
+item[7] = 'MRT'
+item[8] = 'SERIAL_NO'
+item[9] = 'RowNumber'
+item[10] = 'CAT1'
+item[11] = 'CAT2'
+item[12] = 'MEMO_TIME'
+item[13] = 'POI'
+item[14] = 'file'
+item[15] = 'idpt'
+item[16] = 'latitude'
+item[17] = 'xbody'
+item[18] = '_id'
+item[19] = 'avEnd'
+item[20] = 'address'
+
+
+def insertSQL(datanum):
+    for k in range(0, 21):
+        itemContent[k] = data['result']['results'][datanum][item[k]]
+
+    sql = 'select serial_no from taipei where serial_no= %s'
+    val = (itemContent[8], )
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    # print(myresult)
+    if myresult != []:
+        print(myresult)
+        # pass
+    else:
+        # jpgfile處理 -------------------
+        def jpgfileTransform(string):
+            string = string.replace('http', ',http')[1:]
+            files = string.split(',')
+            jpgfile = [''] * len(files)
+            jpgString = ''
+
+            n = 0
+            for k in range(0, len(files)):
+                if files[k][-4:] == '.jpg' or files[k][-4:] == '.JPG':
+                    jpgfile[n] = files[k]
+                    jpgString += ',' + jpgfile[n]
+                    n += 1
+
+            return jpgString[1:]
+
+        itemContent[14] = jpgfileTransform(itemContent[14])
+        # -------------------jpgfile處理
+
+        sql = 'INSERT INTO taipei (info	,stitle	,xpostDate	,longitude	,REF_WP	,avBegin	,langinfo,MRT	,SERIAL_NO	,RowNumber	,CAT1	,CAT2	,MEMO_TIME	,POI	,file	,idpt	,latitude	,xbody	,_id	,avEnd	,address) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s)'
+        val = (tuple(itemContent))
+        mycursor.execute(sql, val)
+        mydb.commit()
+
+
+for alldata in range(0, data['result']['count']):
+    insertSQL(alldata)
