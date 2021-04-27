9305efc37a92a0d9e941e432ec64b3f37574a3a8
Rebecca_mac <bcwu323@gmail.com>
2021-04-27 16:54:18 +0800

    promise-3

--- templates/js/script.js
+++ templates/js/script.js
@@ -1,8 +1,8 @@
 var nextPage = 0
 var keyword = ''
-var url = 'http://0.0.0.0:3000/'
+// var url = 'http://0.0.0.0:3000/'
 // var url = 'http://127.0.0.1:3000/'
-// var url = 'http://18.182.195.43:3000/'
+var url = 'http://18.182.195.43:3000/'
 function getData() {
   return new Promise((resolve, reject) => {
     var req = new XMLHttpRequest()
